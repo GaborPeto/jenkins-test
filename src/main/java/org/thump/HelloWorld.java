7bee7a4d6a51b93eedcb9dddc45088232b4e479e
gabor.peto <gabor.peto@babylonhealth.com>
2017-11-16 15:12:59 +0000

    Commit

---
+++ src/main/java/org/thump/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.thump;
+
+/**
+ * Created by neil on 15/10/16.
+ */
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
